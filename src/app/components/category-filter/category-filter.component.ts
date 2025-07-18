60dc6d5e06eb9adac9dad61a1b498b4718b584eb
shafeeq-ipos <shafeeq@ipossoft.com>
2025-07-18 15:47:16 +0530

    Fix initialization order issues

--- src/app/components/category-filter/category-filter.component.ts
+++ src/app/components/category-filter/category-filter.component.ts
@@ -1,15 +1,19 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-category-filter',
   templateUrl: './category-filter.component.html'
 })
-export class CategoryFilterComponent {
-  categories = this.productService.getCategories();
+export class CategoryFilterComponent implements OnInit {
+  categories: string[] = [];
   @Output() categorySelected = new EventEmitter<string | null>();
 
   constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.categories = this.productService.getCategories();
+  }
 
   selectCategory(category: string | null): void {
     this.categorySelected.emit(category);
