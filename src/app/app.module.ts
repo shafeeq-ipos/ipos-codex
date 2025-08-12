b19d13830b8fd01675e111d2590f57128ef54359
shafeeq-ipos <shafeeq@ipossoft.com>
2025-08-12 16:44:31 +0530

    feat: add login and table selection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -11,6 +11,10 @@ import { CartComponent } from './components/cart/cart.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { CategoryFilterComponent } from './components/category-filter/category-filter.component';
 import { CategoryPipe } from './pipes/category.pipe';
+import { LoginComponent } from './components/login/login.component';
+import { RoomTableComponent } from './components/room-table/room-table.component';
+import { OrderComponent } from './components/order/order.component';
+import { ScreenLockComponent } from './components/screen-lock/screen-lock.component';
 
 @NgModule({
   declarations: [
@@ -21,11 +25,16 @@ import { CategoryPipe } from './pipes/category.pipe';
     CartComponent,
     CheckoutComponent,
     CategoryFilterComponent,
-    CategoryPipe
+    CategoryPipe,
+    LoginComponent,
+    RoomTableComponent,
+    OrderComponent,
+    ScreenLockComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
